608d5573be039b643857a1002214041356a569a7
BeauX <fire-fly_7@hotmail.com>
2024-05-25 15:10:19 -0700

    number entry

--- app.js
+++ app.js
@@ -1,9 +1,43 @@
 /*-------------------------------- Constants --------------------------------*/
+const buttons = document.querySelectorAll('.button');
+console.log('button');
 
+/*buttons.forEach((button) => {
+    button.addEventListener('click', (event) => {
+      console.log(event.target.innerText);  
+    });
+});*/
+const display = document.querySelector('.display');
+let firstValue = 0;
+let secondValue = 0;
+let operatorValue = '';
+const calculator = document.querySelector('#calculator')
+console.log('calculator')
+
+calculator.addEventListener('click', (event) => {
+    if (event.target.classList.contains('number')) {
+        firstValue = event.target.innerText;
+        console.log(firstValue);
+    } else if (event.target.classList.contains ('operator')) {
+        operatorValue = event.target.innerText;
+        console.log(operatorValue);
+    }
+});
 /*-------------------------------- Variables --------------------------------*/
-
+  
 /*------------------------ Cached Element References ------------------------*/
 
 /*----------------------------- Event Listeners -----------------------------*/
 
 /*-------------------------------- Functions --------------------------------*/
+const calculatorOperation = (num1, operator, num2) => {
+        if (operator === '+') {
+            return num1 + num2;
+        } else if (operator === '-') {
+            return num1 - num2;
+        } else if (operator === '*') {
+            return num1 * num2;
+        } else if (operator === '/') {
+            return num1 / num2;
+        }
+    };
