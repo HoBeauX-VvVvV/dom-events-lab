dd388e762e2ae7ffe1403459e724172f60a592c1
BeauX <fire-fly_7@hotmail.com>
2024-05-26 15:22:07 -0700

    formating cleanup

--- app.js
+++ app.js
@@ -4,21 +4,24 @@ const operatorButtons = document.querySelectorAll('.buttonOperator');
 const displayElement = document.querySelector('.display');
 const equalsButton = document.querySelector('.buttonEquals')
 
+/*-------------------------------- Variables --------------------------------*/
+
 let firstValue = '';
 let secondValue = '';
 let operatorValue = '';
 
+/*------------------------ Cached Element References ------------------------*/
+
+/*----------------------------- Event Listeners -----------------------------*/
 
 buttons.forEach((button) => {
-    button.addEventListener('click', (event) => {
-
+    button.addEventListener('click', (event) => { 
     value = event.target.innerText;
     displayElement.textContent += value;
     if (operatorValue === '') {
         firstValue += value; 
         //console.log(`${firstValue} one`);
     } else {
-        //displayElement.textContent = '';
         secondValue += value;
         //console.log(`${secondValue} two`);
     }    
@@ -33,13 +36,23 @@ operatorButtons.forEach((button) => {
         firstValue = '';
         secondValue = '';
         operatorValue = '';
-        displayElement.textContent = '';
     } else {
-    displayElement.textContent = operatorValue;
-    console.log(operatorValue);
+    displayElement.textContent = '';
+    //displayElement.textContent = operatorValue;
+    //console.log(operatorValue);
     }
    });
 });
+
+equalsButton.addEventListener('click', (event) => {
+   const result = calculatorOperation(firstValue, operatorValue, secondValue);
+   displayElement.textContent = result;
+   firstValue = result;
+   secondValue = '';
+   operatorValue = '';
+});
+
+/*-------------------------------- Functions --------------------------------*/
 
 const calculatorOperation = (firstValue, operatorValue, secondValue) => {
     if (operatorValue === '+') {
@@ -53,53 +66,3 @@ const calculatorOperation = (firstValue, operatorValue, secondValue) => {
     }
 };
 
-//console.log(calculatorOperation(15, '*', 15));
-
-equalsButton.addEventListener('click', (event) => {
-   const result = calculatorOperation(firstValue, operatorValue, secondValue);
-   displayElement.textContent = result;
-});
-
-
-
-
-//const calculator = document.querySelector('#calculator')
-//console.dir('equalsButton')
-/*
-calculator.addEventListener('click', (event) => {
-    if (event.target.classList.contains('number')) {
-        Value = event.target.innerText;
-        displayElement.textContent += Value;
-    } else if (event.target.classList.contains ('operator')) {
-        firstValue = displayElement.textContent;
-        operatorValue = event.target.innerText;
-        displayElement.textContent = operatorValue;
-    }
-}); 
-//console.log(firstValue);
-
-
-operatorButtons.forEach(button => {
-operatorButtons.addEventListener('click' , (evt) => {
- const value = button.textContent;
-     if (value === 'C') {
-        clear();
-     } 
-      firstValue = display.textContent;
-    })
-//})
-equalsButton.addEventListener('click', (event) => {
-   equals = event.target.innerText;
-    displayElement.textContent = (equals)
-});
-
-/*-------------------------------- Variables --------------------------------*/
-  
-/*------------------------ Cached Element References ------------------------*/
-
-/*----------------------------- Event Listeners -----------------------------*/
-
-/*-------------------------------- Functions --------------------------------*/
-
-
-//console.log(calculatorOperation(15, '/', 5));
